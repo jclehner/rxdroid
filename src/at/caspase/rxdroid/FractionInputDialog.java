a1013c3fc180a2e669caf50f9774ea8702a20d4d
Joseph C. Lehner <joseph.c.lehner@gmail.com>
2011-09-10 19:11:19 +0000

    Extracted FractionInputDialog from FractionPreference

---
+++ src/at/caspase/rxdroid/FractionInputDialog.java
@@ -0,0 +1,321 @@
+package at.caspase.rxdroid;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.preference.PreferenceManager;
+import android.text.Editable;
+import android.text.InputType;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnLongClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class FractionInputDialog extends AlertDialog implements DialogInterface.OnClickListener, View.OnClickListener, OnLongClickListener, TextWatcher
+{
+	public interface OnFractionSetListener
+	{
+		void onFractionSet(FractionInputDialog dialog, Fraction value);		
+	}
+		
+	private static final String TAG = FractionInputDialog.class.getName();
+	private static final String PREFKEY_MODE = "_fraction_preference_is_in_mixed_number_mode";
+	
+	private EditText mInputNumber;
+	private EditText mInputNumerator;
+	private EditText mInputDenominator;
+	
+	private Button mButtonPlus;
+	private Button mButtonMinus;
+
+	private Button mModeToggler;
+	private boolean mIsInMixedNumberMode;	
+	
+	// the value currently represented in our dialog
+	private Fraction mDialogValue = Fraction.ZERO;
+	// the actual value, copied from mDialogValue if the user presses
+	// the "set" button in the dialog.
+	private Fraction mValue = mDialogValue;
+	
+	private Fraction mLongClickSummand;
+	private OnFractionSetListener mOnFractionSetListener;
+	private boolean mAllowNegativeValues;
+	
+	private boolean mIgnoreTextWatcherEvents = false;
+	
+	private SharedPreferences mSharedPrefs;
+	
+	public FractionInputDialog(Context context, Fraction value, OnFractionSetListener listener) 
+	{
+		super(context);
+		
+		setValue(value);
+		setOnFractionSetListener(listener);
+		setIcon(android.R.drawable.ic_dialog_dialer);
+		
+		LayoutInflater inflater = getLayoutInflater();
+		View view = inflater.inflate(R.layout.fraction_input_dialog, null);
+				
+		mSharedPrefs = PreferenceManager.getDefaultSharedPreferences(getContext());
+		mIsInMixedNumberMode = mSharedPrefs.getBoolean(PREFKEY_MODE, false);
+		
+		mInputNumber = (EditText) view.findViewById(R.id.number);
+		mInputNumber.addTextChangedListener(this);
+		mInputNumber.setVisibility(mIsInMixedNumberMode ? View.VISIBLE : View.GONE);
+		
+		mInputNumerator = (EditText) view.findViewById(R.id.numerator);
+		mInputNumerator.addTextChangedListener(this);
+		
+		mInputDenominator = (EditText) view.findViewById(R.id.denominator);
+		mInputDenominator.addTextChangedListener(this);		
+		
+		mButtonPlus = (Button) view.findViewById(R.id.btn_plus);
+		mButtonPlus.setOnClickListener(this);
+		
+		mButtonMinus = (Button) view.findViewById(R.id.btn_minus);
+		mButtonMinus.setOnClickListener(this);
+
+		mModeToggler = (Button) view.findViewById(R.id.btn_mode_toggle);
+		mModeToggler.setOnClickListener(this);
+		
+		setView(view);
+		
+		setButton(BUTTON_POSITIVE, context.getString(android.R.string.ok), this);
+		setButton(BUTTON_NEGATIVE, context.getString(android.R.string.cancel), (OnClickListener) null);
+		
+		updateInputFields();
+	}
+	
+	public void setValue(Fraction value) 
+	{
+		mDialogValue = value;
+		mValue = value;
+	}
+	
+	public Fraction getValue() {
+		return new Fraction(mValue);
+	}
+	
+	public void setAllowNegativeValues(boolean allowNegativeValues) 
+	{
+		mAllowNegativeValues = allowNegativeValues;
+		
+		if(!allowNegativeValues)
+		{
+			mInputNumber.setInputType(InputType.TYPE_CLASS_NUMBER);
+			mInputNumerator.setInputType(InputType.TYPE_CLASS_NUMBER);			
+		}
+		else
+		{
+			if(mIsInMixedNumberMode)
+			{
+				mInputNumber.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_SIGNED);
+				mInputNumerator.setInputType(InputType.TYPE_CLASS_NUMBER);				
+			}
+			else
+				mInputNumerator.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_SIGNED);			
+		}
+	}
+	
+	public boolean allowsNegativeValues() {
+		return mAllowNegativeValues;
+	}
+	
+	public void setOnFractionSetListener(OnFractionSetListener listener) {
+		mOnFractionSetListener = listener;
+	}
+	
+	public OnFractionSetListener getOnFractionSetListener() {
+		return mOnFractionSetListener;
+	}
+	
+	/**
+	 * Sets a value that is added/subtracted from the current value when
+	 * long-clicking the +/- buttons.
+	 * 
+	 * @param value a fraction. Use <code>null</code> to disable the long-click behaviour.
+	 */	
+	public void setOnLongClickSummand(Fraction summand) 
+	{
+		OnLongClickListener listener = null;
+		
+		if((mLongClickSummand = summand) != null)
+			listener = this;
+		
+		mButtonPlus.setOnLongClickListener(listener);
+		mButtonMinus.setOnLongClickListener(listener);		
+	}
+	
+	@Override
+	public void onClick(DialogInterface dialog, int which)
+	{
+		if(which == BUTTON_POSITIVE)
+		{
+			mValue = mDialogValue;
+			if(mOnFractionSetListener != null)
+				mOnFractionSetListener.onFractionSet(this, mValue);
+			persistFractionInputMode();
+		}		
+	}
+
+	@Override
+	public void onClick(View view)
+	{
+		if(view.getId() == R.id.btn_mode_toggle)
+		{
+			mIsInMixedNumberMode = !mIsInMixedNumberMode;
+			mInputNumber.setVisibility(mIsInMixedNumberMode ? View.VISIBLE : View.GONE);
+						
+			int inputType = InputType.TYPE_CLASS_NUMBER;
+
+			if(!mIsInMixedNumberMode && mAllowNegativeValues)
+				inputType |= InputType.TYPE_NUMBER_FLAG_SIGNED;
+
+			mInputNumerator.setInputType(inputType);
+			updateInputFields();
+			
+		}
+		else if(view.getId() == R.id.btn_plus)
+		{
+			setDialogValue(mDialogValue.plus(1));
+			updateInputFields();
+		}
+		else if(view.getId() == R.id.btn_minus)
+		{
+			setDialogValue(mDialogValue.minus(1));
+			updateInputFields();
+		}
+	}
+	
+	@Override
+	public boolean onLongClick(View view)
+	{
+		if(mLongClickSummand != null)
+		{
+			if(view.getId() == R.id.btn_plus)
+			{
+				setDialogValue(mDialogValue.plus(mLongClickSummand));
+				updateInputFields();
+				return true;
+			}
+			else if(view.getId() == R.id.btn_minus)
+			{
+				setDialogValue(mDialogValue.minus(mLongClickSummand));
+				updateInputFields();
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	
+	@Override
+	public void afterTextChanged(Editable s)
+	{
+		if(mIgnoreTextWatcherEvents)
+			return;
+		
+		int wholeNum, numerator, denominator;
+		
+		try
+		{
+			wholeNum = Integer.parseInt(mInputNumber.getText().toString(), 10);
+			numerator = Integer.parseInt(mInputNumerator.getText().toString(), 10);
+			denominator = Integer.parseInt(mInputDenominator.getText().toString(), 10);
+		}
+		catch(NumberFormatException e)
+		{
+			Log.d(TAG, "afterTextChanged: failed to parse input");
+			return;
+		}
+				
+		if(denominator == 0)
+		{
+			Log.d(TAG, "afterTextChanged: mDialogValue=" + mDialogValue);
+			
+			Toast toast = Toast.makeText(getContext(), "Denominator must not be zero!", Toast.LENGTH_SHORT);
+			// display the Toast on top, as it might get lost when being displayed on the keypad (the default Toast
+			// style and the keypad have very similar colors)
+			toast.setGravity(Gravity.TOP, toast.getXOffset(), toast.getYOffset());
+			toast.show();
+			
+			mInputDenominator.setText(Integer.toString(mDialogValue.getFractionData(false)[2]));
+			mInputDenominator.selectAll();
+			return;
+		}
+				
+		try
+		{
+			setDialogValue(new Fraction(wholeNum, numerator, denominator));
+		}
+		catch(IllegalArgumentException e)
+		{
+			Log.d(TAG, "afterTextChanged: ignoring { " + wholeNum + ", " + numerator  + ", " + denominator + " }");			
+		}
+	}
+
+	@Override
+	public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+	@Override
+	public void onTextChanged(CharSequence s, int start, int before, int count) {}
+	
+	private void setDialogValue(Fraction value)
+	{
+		if(!mAllowNegativeValues && value.compareTo(0) == -1)
+			Log.d(TAG, "setDialogValue: ignoring negative value");
+		else
+		{
+			mDialogValue = value;
+			updateTitle();
+		}
+	}
+
+	private void updateInputFields()
+	{
+		// While updating the EditTexts, the TextWatcher events are handled
+		// after every call to setText(), possibly resulting in a state where
+		// combining the current EditText values obtained by getText() results
+		// in an invalid fraction. To prevent this from happening, any TextWatcher
+		// events in this class should be ignored while mIgnoreTextWatcherEvents is
+		// set to true.
+		mIgnoreTextWatcherEvents = true;
+		
+		int[] fractionData = mDialogValue.getFractionData(mIsInMixedNumberMode);
+
+		if (mIsInMixedNumberMode)
+			mInputNumber.setText(Integer.toString(fractionData[0]));
+		else
+			mInputNumber.setText("0");
+
+		mInputNumerator.setText(Integer.toString(fractionData[1]));
+		mInputDenominator.setText(Integer.toString(fractionData[2]));
+		
+		mIgnoreTextWatcherEvents = false;
+		
+		if(!mAllowNegativeValues && mLongClickSummand == null && mDialogValue.minus(1).compareTo(0) == -1)
+			mButtonMinus.setEnabled(false);
+		else
+			mButtonMinus.setEnabled(true);
+	}
+	
+	private void updateTitle() 
+	{
+		Log.d(TAG, "updateTitle: mDialogValue=" + mDialogValue);
+		setTitle(mDialogValue.toString());
+	}
+	
+	private void persistFractionInputMode()
+	{
+		Editor editor = mSharedPrefs.edit();
+		editor.putBoolean(PREFKEY_MODE, mIsInMixedNumberMode);
+		editor.commit();
+	}
+}
--- src/at/caspase/rxdroid/FractionPreference.java
+++ src/at/caspase/rxdroid/FractionPreference.java
@@ -1,320 +1,65 @@
 
 package at.caspase.rxdroid;
 
-import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
-import android.content.SharedPreferences;
-import android.content.SharedPreferences.Editor;
-import android.os.Parcelable;
-import android.preference.DialogPreference;
-import android.preference.PreferenceManager;
-import android.text.Editable;
-import android.text.InputType;
-import android.text.TextWatcher;
+import android.content.res.TypedArray;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceClickListener;
 import android.util.AttributeSet;
 import android.util.Log;
-import android.view.HapticFeedbackConstants;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.View.OnLongClickListener;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
+import at.caspase.rxdroid.FractionInputDialog.OnFractionSetListener;
 
-public class FractionPreference extends DialogPreference implements OnClickListener, OnLongClickListener, TextWatcher
+public class FractionPreference extends Preference implements OnPreferenceClickListener, OnFractionSetListener
 {
 	private static final String TAG = FractionPreference.class.getName();
-	private static final String PREFKEY_MODE = "_fraction_preference_is_in_mixed_number_mode";
 	
-	private EditText mInputNumber;
-	private EditText mInputNumerator;
-	private EditText mInputDenominator;
+	private Fraction mValue;
 	
-	private Button mButtonPlus;
-	private Button mButtonMinus;
-
-	private Button mModeToggler;
-	private boolean mIsInMixedNumberMode;
-
-	// the value currently represented in our dialog
-	private Fraction mDialogValue = Fraction.ZERO;
-	// the actual value, copied from mDialogValue if the user presses
-	// the "set" button in the dialog.
-	private Fraction mValue = mDialogValue;
-	
-	private Fraction mLongClickSummand;
-	private boolean mAllowNegativeValues;
-	
-	private boolean mIgnoreTextWatcherEvents = false;
-	
-	private SharedPreferences mSharedPrefs;
-
 	public FractionPreference(Context context, AttributeSet attrs) {
 		this(context, attrs, 0);
 	}
-
+	
 	public FractionPreference(Context context, AttributeSet attrs, int defStyle) 
 	{
 		super(context, attrs, defStyle);
 		
-		setDialogIcon(android.R.drawable.ic_dialog_dialer);
-		
-		mSharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
-		mIsInMixedNumberMode = mSharedPrefs.getBoolean(PREFKEY_MODE, false);
-	}
-	
-	public void setValue(Fraction value) {
-		mDialogValue = mValue = value;
-	}
+		mValue = Fraction.decode(getPersistedString("0"));
+		setOnPreferenceClickListener(this);
+	}	
 	
 	public Fraction getValue() {
-		return new Fraction(mValue);
-	}
-	
-	public void setAllowNegativeValues(boolean allowNegativeValues) 
-	{
-		mAllowNegativeValues = allowNegativeValues;
-		
-		if(!allowNegativeValues)
-		{
-			mInputNumber.setInputType(InputType.TYPE_CLASS_NUMBER);
-			mInputNumerator.setInputType(InputType.TYPE_CLASS_NUMBER);			
-		}
-		else
-		{
-			if(mIsInMixedNumberMode)
-			{
-				mInputNumber.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_SIGNED);
-				mInputNumerator.setInputType(InputType.TYPE_CLASS_NUMBER);				
-			}
-			else
-				mInputNumerator.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_SIGNED);			
-		}
-	}
-	
-	public boolean getAllowNegativeValues() {
-		return mAllowNegativeValues;
-	}
-	
-	/**
-	 * Sets a value that is added/subtracted from the current value when
-	 * long-clicking the +/- buttons.
-	 * 
-	 * @param value a fraction. Use <code>null</code> to disable the long-click behaviour.
-	 */	
-	public void setOnLongClickSummand(Fraction summand) 
-	{
-		OnLongClickListener listener = null;
-		
-		if((mLongClickSummand = summand) != null)
-			listener = this;
-		
-		mButtonPlus.setOnLongClickListener(listener);
-		mButtonMinus.setOnLongClickListener(listener);		
+		return mValue;
 	}
 	
 	@Override
-	public CharSequence getSummary()
-	{
+	public CharSequence getSummary() {
 		return mValue.toString();
 	}
+	
+	@Override
+	public boolean onPreferenceClick(Preference preference)
+	{
+		FractionInputDialog dialog = new FractionInputDialog(getContext(), mValue, this);
+		dialog.show();
+		return true;
+	}
 
 	@Override
-	public void onClick(View view)
+	public void onFractionSet(FractionInputDialog dialog, Fraction value)
 	{
-		if(view.getId() == R.id.btn_mode_toggle)
-		{
-			mIsInMixedNumberMode = !mIsInMixedNumberMode;
-			mInputNumber.setVisibility(mIsInMixedNumberMode ? View.VISIBLE : View.GONE);
-						
-			int inputType = InputType.TYPE_CLASS_NUMBER;
-
-			if(!mIsInMixedNumberMode && mAllowNegativeValues)
-				inputType |= InputType.TYPE_NUMBER_FLAG_SIGNED;
-
-			mInputNumerator.setInputType(inputType);
-			onDialogValueChanged();
-			
-		}
-		else if(view.getId() == R.id.btn_plus)
-		{
-			mDialogValue = mDialogValue.plus(1);
-			onDialogValueChanged();
-		}
-		else if(view.getId() == R.id.btn_minus)
-		{
-			setDialogValue(mDialogValue.minus(1));
-			onDialogValueChanged();
-		}
+		if(shouldPersist())
+			persistString(value.toString());
+		
+		mValue = dialog.getValue();		
+		setSummary(mValue.toString());
+		notifyChanged();
 	}
 	
 	@Override
-	public boolean onLongClick(View view)
+	protected Object onGetDefaultValue(TypedArray a, int index) 
 	{
-		if(mLongClickSummand != null)
-		{
-			if(view.getId() == R.id.btn_plus)
-			{
-				mDialogValue = mDialogValue.plus(mLongClickSummand);
-				onDialogValueChanged();
-				return true;
-			}
-			else if(view.getId() == R.id.btn_minus)
-			{
-				setDialogValue(mDialogValue.minus(mLongClickSummand));
-				onDialogValueChanged();
-				return true;
-			}
-		}
-		
-		return false;
-	}
-
-	@Override
-	public void afterTextChanged(Editable s)
-	{
-		if(mIgnoreTextWatcherEvents)
-			return;
-		
-		int wholeNum, numerator, denominator;
-		
-		try
-		{
-			wholeNum = Integer.parseInt(mInputNumber.getText().toString(), 10);
-			numerator = Integer.parseInt(mInputNumerator.getText().toString(), 10);
-			denominator = Integer.parseInt(mInputDenominator.getText().toString(), 10);
-		}
-		catch(NumberFormatException e)
-		{
-			Log.d(TAG, "afterTextChanged: failed to parse input");
-			return;
-		}
-				
-		if(denominator == 0)
-		{
-			Log.d(TAG, "afterTextChanged: mDialogValue=" + mDialogValue);
-			
-			Toast.makeText(getContext(), "Denominator must not be zero!", Toast.LENGTH_SHORT).show();
-			mInputDenominator.setText(Integer.toString(mDialogValue.getFractionData(false)[2]));
-			mInputDenominator.selectAll();
-			return;
-		}
-				
-		try
-		{
-			mDialogValue = new Fraction(wholeNum, numerator, denominator);
-		}
-		catch(IllegalArgumentException e)
-		{
-			Log.d(TAG, "afterTextChanged: ignoring { " + wholeNum + ", " + numerator  + ", " + denominator + " }");			
-		}
-	}
-
-	@Override
-	public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
-
-	@Override
-	public void onTextChanged(CharSequence s, int start, int before, int count) {}
-	
-	@Override
-	protected void onPrepareDialogBuilder(AlertDialog.Builder builder)
-	{
-		super.onPrepareDialogBuilder(builder);
-		
-		DialogInterface.OnClickListener listener = new DialogInterface.OnClickListener() {
-			
-			@Override
-			public void onClick(DialogInterface dialog, int which)
-			{
-				if(which == DialogInterface.BUTTON_POSITIVE)
-				{
-					mValue = mDialogValue;
-					persistFractionInputMode();
-					setSummary(mValue.toString());
-					notifyChanged();
-				}
-			}
-		};
-		
-		builder.setPositiveButton("Set", listener);
-		builder.setNegativeButton("Cancel", null);
-	}
-
-	@Override
-	protected View onCreateDialogView()
-	{		
-		LayoutInflater inflater = LayoutInflater.from(getContext());
-		View view = inflater.inflate(R.layout.fraction_preference, null);
-
-		mInputNumber = (EditText) view.findViewById(R.id.number);
-		mInputNumber.addTextChangedListener(this);
-		mInputNumber.setVisibility(mIsInMixedNumberMode ? View.VISIBLE : View.GONE);
-		
-		mInputNumerator = (EditText) view.findViewById(R.id.numerator);
-		mInputNumerator.addTextChangedListener(this);
-		
-		mInputDenominator = (EditText) view.findViewById(R.id.denominator);
-		mInputDenominator.addTextChangedListener(this);		
-		
-		mButtonPlus = (Button) view.findViewById(R.id.btn_plus);
-		mButtonPlus.setOnClickListener(this);
-		
-		mButtonMinus = (Button) view.findViewById(R.id.btn_minus);
-		mButtonMinus.setOnClickListener(this);
-
-		mModeToggler = (Button) view.findViewById(R.id.btn_mode_toggle);
-		mModeToggler.setOnClickListener(this);
-				
-		// XXX
-		setAllowNegativeValues(true);
-
-		onDialogValueChanged();
-		
-		return view;
-	}
-	
-	private void setDialogValue(Fraction value)
-	{
-		if(!mAllowNegativeValues && value.compareTo(0) == -1)
-			Log.d(TAG, "setDialogValue: ignoring negative value");
-		else
-			mDialogValue = value;		
-	}
-
-	private void onDialogValueChanged()
-	{
-		// While updating the EditTexts, the TextWatcher events are handled
-		// after every call to setText(), possibly resulting in a state where
-		// combining the current EditText values obtained by getText() results
-		// in an invalid fraction. To prevent this from happening, any TextWatcher
-		// events in this class should be ignored while mIgnoreTextWatcherEvents is
-		// set to true.
-		mIgnoreTextWatcherEvents = true;
-		
-		int[] fractionData = mDialogValue.getFractionData(mIsInMixedNumberMode);
-
-		if (mIsInMixedNumberMode)
-			mInputNumber.setText(Integer.toString(fractionData[0]));
-		else
-			mInputNumber.setText("0");
-
-		mInputNumerator.setText(Integer.toString(fractionData[1]));
-		mInputDenominator.setText(Integer.toString(fractionData[2]));
-		
-		mIgnoreTextWatcherEvents = false;
-		
-		if(!mAllowNegativeValues && mLongClickSummand == null && mDialogValue.minus(1).compareTo(0) == -1)
-			mButtonMinus.setEnabled(false);
-		else
-			mButtonMinus.setEnabled(true);
-	}
-	
-	private void persistFractionInputMode()
-	{
-		Editor editor = mSharedPrefs.edit();
-		editor.putBoolean(PREFKEY_MODE, mIsInMixedNumberMode);
-		editor.commit();
+		String value = a.getString(index);
+		Log.d(TAG, "onGetDefaultValue: value=" + value);
+		return value != null ? value : "0"; 
 	}
 }
