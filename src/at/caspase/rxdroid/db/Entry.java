c6bf606147c7a490b629a332bf5159a5da3c90eb
Joseph C. Lehner <joseph.c.lehner@gmail.com>
2011-10-16 14:39:49 +0000

    added missing GPL headers

--- src/at/caspase/rxdroid/db/Entry.java
+++ src/at/caspase/rxdroid/db/Entry.java
@@ -1,3 +1,24 @@
+/**
+ * Copyright (C) 2011 Joseph Lehner <joseph.c.lehner@gmail.com>
+ *
+ * This file is part of RxDroid.
+ *
+ * RxDroid is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * RxDroid is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with RxDroid.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ */
+
 package at.caspase.rxdroid.db;
 
 import java.io.Serializable;
@@ -31,12 +52,28 @@ public abstract class Entry implements Serializable
 	@Override
 	public abstract int hashCode();
 
-	public int getId() {
+	/**
+	 * Returns the auto-generated database ID.
+	 */
+	public final int getId() {
 		return id;
 	}
 
-	public void setId(int id) {
+	/**
+	 * Sets the database ID.
+	 * <p>
+	 * As the ID is automatically generated, there's no need to use this function,
+	 * apart from a few special cases.
+	 */
+	public final void setId(int id) {
 		this.id = id;
+	}
+	
+	/**
+	 * Converts this Entry to an instance usable by the current database version.
+	 */
+	public Entry convert() {
+		throw new UnsupportedOperationException();
 	}
 
 	public static<T extends Entry> T findInCollection(Collection<T> collection, int id)
