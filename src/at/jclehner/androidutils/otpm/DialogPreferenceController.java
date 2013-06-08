fe373a0513e2b3e18c53d8666f132bc67519cf33
Joseph C. Lehner <joseph.c.lehner@gmail.com>
2013-06-08 14:39:02 +0000

    added missing file that caused build errors

---
+++ src/at/jclehner/androidutils/otpm/DialogPreferenceController.java
@@ -0,0 +1,16 @@
+package at.jclehner.androidutils.otpm;
+
+import android.preference.DialogPreference;
+
+public class abstract DialogPreferenceController<P extends DialogPreference, T> extends PreferenceController<P, T>
+{
+	public void initPreference(P preference, T fieldValue)
+	{
+		if(hideWidget())
+			preference.setWidgetLayoutResource(0);
+	}
+
+	protected boolean hideWidget() {
+		return true;
+	}
+}
