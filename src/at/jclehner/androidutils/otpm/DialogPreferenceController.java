12a0f65f80e4ff7ef5f3001d8c0b685c1a14b011
Joseph C. Lehner <joseph.c.lehner@gmail.com>
2013-06-08 14:39:30 +0000

    fixed typo

--- src/at/jclehner/androidutils/otpm/DialogPreferenceController.java
+++ src/at/jclehner/androidutils/otpm/DialogPreferenceController.java
@@ -2,7 +2,7 @@ package at.jclehner.androidutils.otpm;
 
 import android.preference.DialogPreference;
 
-public class abstract DialogPreferenceController<P extends DialogPreference, T> extends PreferenceController<P, T>
+public abstract class DialogPreferenceController<P extends DialogPreference, T> extends PreferenceController<P, T>
 {
 	public void initPreference(P preference, T fieldValue)
 	{
