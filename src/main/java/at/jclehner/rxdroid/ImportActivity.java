d4e18ff41c7b996f4badcb475a5e2fcc08f85982
Joseph C. Lehner <joseph.c.lehner@gmail.com>
2013-10-12 17:30:52 +0200

    Add ImportActivity

---
+++ src/main/java/at/jclehner/rxdroid/ImportActivity.java
@@ -0,0 +1,17 @@
+package at.jclehner.rxdroid;
+
+import android.os.Bundle;
+
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.google.gson.Gson;
+
+import java.io.FileReader;
+
+public class ImportActivity extends SherlockFragmentActivity
+{
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+	}
+}
