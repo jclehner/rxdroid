8aafe4631e26fee22c12ee4fdd14ec9674ceff37
Joseph C. Lehner <joseph.c.lehner@gmail.com>
2013-11-29 17:05:08 +0100

    Handle formatArgs and android:string in RefString

--- src/main/java/at/jclehner/androidutils/RefString.java
+++ src/main/java/at/jclehner/androidutils/RefString.java
@@ -9,12 +9,29 @@ public class RefString
 	public static String resolve(Context context, CharSequence text)
 	{
 		final StringBuilder sb = new StringBuilder(text);
-		final String defPackage = context.getApplicationContext().getPackageName();
+		final String appPackage = context.getApplicationContext().getPackageName();
+
+		resolvePrefix(context, sb, "string", appPackage);
+		resolvePrefix(context, sb, "android:string", "android");
+
+		return sb.toString();
+	}
+
+	public static String resolve(Context context, int textResId) {
+		return resolve(context, context.getString(textResId));
+	}
+
+	public static String resolve(Context context, int textResId, Object... formatArgs) {
+		return resolve(context, context.getString(textResId, formatArgs));
+	}
+
+	private static final void resolvePrefix(Context context, StringBuilder sb, String prefix, String defPackage)
+	{
 		final Resources res = context.getResources();
 
 		int beg;
 
-		while((beg = sb.indexOf("[@string/")) != -1)
+		while((beg = sb.indexOf("[@" + prefix + "/")) != -1)
 		{
 			int end = sb.indexOf("]", beg);
 			String name = sb.substring(beg + 2, end);
@@ -24,12 +41,6 @@ public class RefString
 
 			sb.replace(beg, end + 1, context.getString(resId));
 		}
-
-		return sb.toString();
-	}
-
-	public static String resolve(Context context, int textResId) {
-		return resolve(context, context.getString(textResId));
 	}
 
 	private RefString() {}
