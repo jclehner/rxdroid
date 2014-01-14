e4cd540533335f639659dbe4d6a50080bac98e7d
Joseph C. Lehner <joseph.c.lehner@gmail.com>
2014-01-14 22:35:39 +0100

    Add and use DrugListPagerFragment

--- src/main/java/at/jclehner/rxdroid/DrugListActivity2.java
+++ src/main/java/at/jclehner/rxdroid/DrugListActivity2.java
@@ -14,12 +14,15 @@ public class DrugListActivity2 extends SherlockFragmentActivity
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
-		Components.onCreate(this, Components.NO_DATABASE_INIT);
+		Components.onCreateActivity(this, Components.NO_DATABASE_INIT);
 
 		super.onCreate(savedInstanceState);
 
-		DrugListFragment f = DrugListFragment.newInstance(DateTime.today(), Patient.DEFAULT_PATIENT_ID,
-				Settings.getDoseTimeInfo());
+		final DrugListPagerFragment f = new DrugListPagerFragment();
+		final Bundle args = new Bundle();
+		args.putSerializable(DrugListFragment.ARG_DATE, DateTime.today());
+		args.putInt(DrugListFragment.ARG_PATIENT_ID, Patient.DEFAULT_PATIENT_ID);
+		f.setArguments(args);
 
 		getSupportFragmentManager().beginTransaction().add(android.R.id.content, f).commit();
 	}
