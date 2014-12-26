19848bfe97e1003935a1b17b2617bec0675857f5
Joseph C. Lehner <joseph.c.lehner@gmail.com>
2014-12-26 17:25:51 +0200

    Perform db upgrades one at a time

--- src/main/java/at/jclehner/rxdroid/db/DatabaseUpgrader.java
+++ src/main/java/at/jclehner/rxdroid/db/DatabaseUpgrader.java
@@ -1,6 +1,7 @@
 package at.jclehner.rxdroid.db;
 
 import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
 
 import com.j256.ormlite.support.ConnectionSource;
 import com.j256.ormlite.support.DatabaseConnection;
@@ -11,6 +12,8 @@ import java.sql.SQLException;
 
 public class DatabaseUpgrader implements Closeable
 {
+	private static final String TAG = DatabaseUpgrader.class.getSimpleName();
+
 	private final SQLiteDatabase mDb;
 	private final ConnectionSource mCs;
 	private final DatabaseConnection mDc;
@@ -24,50 +27,70 @@ public class DatabaseUpgrader implements Closeable
 
 	public void onUpgrade(int oldVersion, int newVersion) throws SQLException
 	{
-		if(oldVersion < 54)
+		for(int version = oldVersion + 1; version <= newVersion; ++version)
 		{
-			throw new DatabaseHelper.DatabaseError(DatabaseHelper.DatabaseError.E_UPGRADE,
-					"Unsupported database version");
-		}
-
-		if(oldVersion < 55)
-			execute("UPDATE [drugs] SET [sortRank]=" + Integer.MAX_VALUE);
-
-		if(oldVersion < 56)
-		{
-			TableUtils.createTableIfNotExists(mCs, Patient.class);
-			execute("INSERT INTO [patients] ( [name], [id] ) VALUES ( NULL, " + Patient.DEFAULT_PATIENT_ID);
-		}
-
-		if(oldVersion < 57)
-			execute("ALTER TABLE [drugs] ADD COLUMN [lastScheduleUpdateDate] VARCHAR DEFAULT NULL");
-
-		if(oldVersion < 58)
-		{
-			// Theoretically, we should create the [schedules] and [schedulepart] tables, but since
-			// these are disabled for now, this upgrade is a no-op. We set any schedule_id
-		}
-
-		if(oldVersion < 59)
-		{
-			execute("DROP TABLE [schedules]");
-			execute("DROP TABLE [schedulepart]");
-
-			execute("ALTER TABLE [intake] RENAME TO [dose_events]");
-
-			execute("ALTER TABLE [drugs] ADD COLUMN [autoAddDoseEvents] SMALLINT");
-			execute("UPDATE [drugs] SET [autoAddDoseEvents]=[autoAddIntakes]");
-
-			execute("ALTER TABLE [drugs] ADD COLUMN [lastAutoDoseEventCreationDate] VARCHAR");
-			execute("UPDATE [drugs] SET [lastAutoDoseEventCreationDate]=[lastAutoIntakeCreationDate]");
-
-			execute("ALTER TABLE [drugs] ADD COLUMN [expirationDate] VARCHAR");
+			try
+			{
+				Log.i(TAG, "Upgrading to v" + version);
+				upgradeTo(version);
+				mDb.setVersion(version);
+			}
+			catch(SQLException e)
+			{
+				mDb.setVersion(version - 1);
+				throw new DatabaseHelper.DatabaseError(DatabaseHelper.DatabaseError.E_UPGRADE, e);
+			}
 		}
 	}
 
 	@Override
 	public void close() {
 		mDc.closeQuietly();
+	}
+
+	private void upgradeTo(int version) throws SQLException
+	{
+		switch(version)
+		{
+			case 55:
+				execute("UPDATE [drugs] SET [sortRank]=" + Integer.MAX_VALUE);
+				break;
+
+			case 56:
+				// TODO this must be changed if the [patient] table is ever changed
+				TableUtils.createTableIfNotExists(mCs, Patient.class);
+				execute("INSERT INTO [patients] ( [name], [id] ) VALUES ( NULL, " + Patient.DEFAULT_PATIENT_ID);
+				break;
+
+			case 57:
+				execute("ALTER TABLE [drugs] ADD COLUMN [lastScheduleUpdateDate] VARCHAR DEFAULT NULL");
+				break;
+
+			case 58:
+				// Theoretically, we should create the [schedules] and [schedulepart] tables, but since
+				// these are disabled for now, this upgrade is a no-op.
+				break;
+
+			case 59:
+				execute("DROP TABLE [schedules]");
+				execute("DROP TABLE [schedulepart]");
+
+				execute("ALTER TABLE [intake] RENAME TO [dose_events]");
+				// Rename autoAddIntakes -> hasAutoDoseEvents
+				execute("ALTER TABLE [drugs] ADD COLUMN [hasAutoDoseEvents] SMALLINT");
+				execute("UPDATE [drugs] SET [hasAutoDoseEvents]=[autoAddIntakes]");
+				// Rename lastAutoIntakeCreationDate -> lastAutoDoseEventCreationDate
+				execute("ALTER TABLE [drugs] ADD COLUMN [lastAutoDoseEventCreationDate] VARCHAR");
+				execute("UPDATE [drugs] SET [lastAutoDoseEventCreationDate]=[lastAutoIntakeCreationDate]");
+
+				execute("ALTER TABLE [drugs] ADD COLUMN [expirationDate] VARCHAR");
+
+				break;
+
+			default:
+				throw new DatabaseHelper.DatabaseError(DatabaseHelper.DatabaseError.E_UPGRADE,
+						"Unsupported database version " + version);
+		}
 	}
 
 	private int execute(String statement) throws SQLException {
