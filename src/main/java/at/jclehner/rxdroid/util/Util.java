08ea9ab85f260ce38ff9fdc2318de1fc9d740245
Joseph C. Lehner <joseph.c.lehner@gmail.com>
2015-09-12 10:35:36 +0200

    Use unicode chars for fifths and sixths on >= Lollipop

--- src/main/java/at/jclehner/rxdroid/util/Util.java
+++ src/main/java/at/jclehner/rxdroid/util/Util.java
@@ -640,18 +640,18 @@ public final class Util
 		// while Jellybean converts
 		// them to their simple counterparts (i.e. 1/5 instead of ⅕)
 
-		if(frac.isInteger() || (frac.denominator() > 4 && frac.denominator() != 8))
-			return frac.toString();
+		if(!frac.isInteger() && (frac.denominator() <= (Version.SDK_IS_LOLLIPOP_OR_NEWER ? 6 : 4) || frac.denominator() == 8))
+		{
+			final int[] data = frac.getFractionData(true);
+			// numerator minus integer component!
+			final int wholeNum = data[0];
+			final int numerator = data[1];
+			final int denominator = data[2];
 
-		final int[] data = frac.getFractionData(true);
-		// numerator minus integer component!
-		final int wholeNum = data[0];
-		final int numerator = data[1];
-		final int denominator = data[2];
-
-		final String pretty = PRETTY_FRACTIONS[denominator - 2][numerator - 1];
-		if(pretty != null)
-			return wholeNum != 0 ? (wholeNum + pretty) : pretty;
+			final String pretty = PRETTY_FRACTIONS[denominator - 2][numerator - 1];
+			if(pretty != null)
+				return wholeNum != 0 ? (wholeNum + pretty) : pretty;
+		}
 
 		return frac.toString();
 	}
