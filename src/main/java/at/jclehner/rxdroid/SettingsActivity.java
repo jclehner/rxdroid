6291ea4701c6fe088bb2d7879875f13a73e863e8
Joseph C. Lehner <joseph.c.lehner@gmail.com>
2023-08-15 12:52:48 +0200

    Update displayed copyright dates

--- src/main/java/at/jclehner/rxdroid/SettingsActivity.java
+++ src/main/java/at/jclehner/rxdroid/SettingsActivity.java
@@ -179,7 +179,7 @@ public class SettingsActivity extends AppCompatActivity
 				}
 
 				sb.append("\n" +
-						"Copyright (C) 2011-2021 Joseph C. Lehner\n" +
+						"Copyright (C) 2011-2023 Joseph C. Lehner\n" +
 						"<joseph.c.lehner@gmail.com>");
 
 				final String translator = getString(R.string.translator);
